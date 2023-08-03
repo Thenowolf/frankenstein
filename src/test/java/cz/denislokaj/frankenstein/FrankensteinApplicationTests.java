aa97c90283aa373e8851537a448860eb7ac38c3a
Denis Lokaj <nowolf@seznam.cz>
2023-08-03 15:46:37 +0200

    Project created

---
+++ src/test/java/cz/denislokaj/frankenstein/FrankensteinApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.denislokaj.frankenstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankensteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
